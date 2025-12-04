4ae22d76611779abf04efd505c552af728036753
FridaLy <payanfridaliz@gmail.com>
2025-12-03 21:57:36 -0600

    Logica

--- src/main/java/com/example/voluntariado_p2sem/jpa/model/Sede.java
+++ src/main/java/com/example/voluntariado_p2sem/jpa/model/Sede.java
@@ -1,4 +1,55 @@
 package com.example.voluntariado_p2sem.jpa.model;
 
-public class Sede {
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "sede")
+public class Sede implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sede")
+    private Integer idSede;
+
+    @Column(name = "id_municipio")
+    private Integer idMunicipio;  
+
+    @Column(name = "nombresede", length = 150)
+    private String nombreSede;
+
+    @Column(name = "direccionsede", length = 255)
+    private String direccionSede;
+
+    public Integer getIdSede() {
+        return idSede;
+    }
+
+    public void setIdSede(Integer idSede) {
+        this.idSede = idSede;
+    }
+
+    public Integer getIdMunicipio() {
+        return idMunicipio;
+    }
+
+    public void setIdMunicipio(Integer idMunicipio) {
+        this.idMunicipio = idMunicipio;
+    }
+
+    public String getNombreSede() {
+        return nombreSede;
+    }
+
+    public void setNombreSede(String nombreSede) {
+        this.nombreSede = nombreSede;
+    }
+
+    public String getDireccionSede() {
+        return direccionSede;
+    }
+
+    public void setDireccionSede(String direccionSede) {
+        this.direccionSede = direccionSede;
+    }
 }
--- src/main/java/com/example/voluntariado_p2sem/jpa/model/Semestre.java
+++ src/main/java/com/example/voluntariado_p2sem/jpa/model/Semestre.java
@@ -1,4 +1,46 @@
 package com.example.voluntariado_p2sem.jpa.model;
 
-public class Semestre {
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "semestre")
+public class Semestre implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_semestre")
+    private Integer idSemestre;
+
+    @Column(name = "nosemestre")
+    private Integer noSemestre;
+
+    @Column(name = "aniosemestre")
+    private Integer anioSemestre;
+
+    // Getters y setters
+
+    public Integer getIdSemestre() {
+        return idSemestre;
+    }
+
+    public void setIdSemestre(Integer idSemestre) {
+        this.idSemestre = idSemestre;
+    }
+
+    public Integer getNoSemestre() {
+        return noSemestre;
+    }
+
+    public void setNoSemestre(Integer noSemestre) {
+        this.noSemestre = noSemestre;
+    }
+
+    public Integer getAnioSemestre() {
+        return anioSemestre;
+    }
+
+    public void setAnioSemestre(Integer anioSemestre) {
+        this.anioSemestre = anioSemestre;
+    }
 }
